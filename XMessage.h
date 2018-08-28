081a922a6c1ccb20cc4b2fcc6dad23263456adf9
flowingbrain <42708521+flowingbrain@users.noreply.github.com>
2018-08-28 13:44:58 +0800

    Update XMessage.h

--- XMessage.h
+++ XMessage.h
@@ -1,3 +1,5 @@
+/*
+
 //
 // Created by skyzheng on 8/23/18.
 //
@@ -20,6 +22,29 @@ namespace XQ
         char name[MESSAGE_NAME_LENGTH];
         char data[MESSAGE_DATA_LENGTH];
     };
+}
+
+#endif //XQSERVER_XMESSAGE_H
+
+
+*/
+
+#ifndef XQSERVER_XMESSAGE_H
+#define XQSERVER_XMESSAGE_H
+
+namespace XQ
+{
+
+	const int 
+
+	struct message
+	{
+		int type;
+		long time;
+		char toUser[32];
+		char fromUser[32];
+		char data[1024];
+	};
 }
 
 #endif //XQSERVER_XMESSAGE_H
