6ca527488d6b70a07b068c91316ecbab5a1d9b12
jiang nao <318007543@qq.com>
2018-08-28 15:16:26 +0800

    into functions

--- database.cpp
+++ database.cpp
@@ -1,42 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h> 
+#include <string.h>
+
 
 class sql
 {
 private:
+
 	sqlite3 * db;
+	string dbpath;
 	char *zErrMsg = 0;
 	int  rc;
 	char *sql;
 
 
 public:
-	static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+
+	static int callback(void *NotUsed, int argc, char **argv, char **azColName) 
+	{
 		int i;
-		for (i = 0; i<argc; i++) {
+		for (i = 0; i<argc; i++) 
+		{
 			printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
 		}
 		printf("\n");
 		return 0;
 	}
 
-	int main(int argc, char* argv[])
+	int setup()
 	{
 		
 		/* Open database */
 		rc = sqlite3_open("test.db", &db);
-		if (rc) {
+		if (rc) 
+		{
 			fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 			exit(0);
 		}
-		else {
+		else 
+		{
 			fprintf(stdout, "Opened database successfully\n");
 		}
 
 		
 		/* Create SQL statement */
-
 		/*
 		sql = "CREATE TABLE COMPANY("  \
 			"ID INT PRIMARY KEY     NOT NULL," \
@@ -48,15 +56,22 @@ public:
 
 		/* Execute SQL statement */
 		rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-		if (rc != SQLITE_OK) {
+		if (rc != SQLITE_OK) 
+		{
 			fprintf(stderr, "SQL error: %s\n", zErrMsg);
 			sqlite3_free(zErrMsg);
 		}
-		else {
+		else 
+		{
 			fprintf(stdout, "Table created successfully\n");
 		}
 		sqlite3_close(db);
 		return 0;
+	}
+
+	void setsqlstr(string sql)
+	{
+		char*(sql);
 	}
 
 };
